768157cb8b41e37e64ab9d5b557793d2fbfb94e0
Luz Gutierrez <mili01gm@gmail.com>
2017-05-29 18:47:10 -0500

    Nav donce. Footer remains with uncomplete style.

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,3 +1,11 @@
+var menuXs = document.getElementById("call-menu");
+var onlyXs = document.getElementById("menu-xs");
+
+menuXs.addEventListener("click", function() {
+    onlyXs.classList.toggle("none");
+});
+
+
 function initMap() {
     var labCoords = { lat: -12.1191427, lng: -77.0349046 };
     var map = new google.maps.Map(document.getElementById("map"), {
